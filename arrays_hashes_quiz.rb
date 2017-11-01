695f960ddad2690556054d149345f87106f07ddc
eliotrhys <eliot@eliotshort.co.uk>
2017-11-01 20:21:23 +0000

    Last bit to go now.

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -59,16 +59,32 @@ lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'P
 
 my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
 
+### DONE -----------------------------------------------------------
 # 1. How would you return the string `"One"`?
-p [:my_hash][1]
+# p my_hash[1]
+### DONE -----------------------------------------------------------
 
+### DONE -----------------------------------------------------------
 # 2. How would you return the string `"Two"`?
+# p my_hash[:two]
+### DONE -----------------------------------------------------------
 
+### DONE -----------------------------------------------------------
 # 3. How would you return the number `2`?
+# p my_hash["two"]
+### DONE -----------------------------------------------------------
 
+### DONE -----------------------------------------------------------
 # 4. How would you add `{3 => "Three"}` to the hash?
+# my_hash[3] = "Three"
+# p my_hash
+### DONE -----------------------------------------------------------
 
+### DONE -----------------------------------------------------------
 # 5. How would you add `{:four => 4}` to the hash?
+# my_hash[:four] = 4
+# p my_hash
+### DONE -----------------------------------------------------------
 
 ### C. Given the following data structure:
 
@@ -104,15 +120,29 @@ users = {
   }
 }
 
+### DONE -----------------------------------------------------------
 # 1. Return Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+# p users["Jonathan"][:twitter]
+### DONE -----------------------------------------------------------
 
+### DONE -----------------------------------------------------------
 # 2. Return Erik's hometown
+# p users["Erik"][:home_town]
+### DONE -----------------------------------------------------------
 
+### DONE -----------------------------------------------------------
 # 3. Return the array of Erik's favourite numbers
+# p users["Erik"][:favourite_numbers]
+### DONE -----------------------------------------------------------
 
+### DONE -----------------------------------------------------------
 # 4. Return the type of Avril's pet Colin
+# p users["Avril"][:pets]["colin"]
+### DONE -----------------------------------------------------------
 
 # 5. Return the smallest of Erik's favourite numbers
+p users["Erik"][:favourite_numbers]
+
 
 # 6. Add the number `7` to Erik's favourite numbers
 
